8a59389a0db3dcfae0b4913ad10a4cc8b82116d6
henrique <you@example.com>
2018-02-12 20:45:38 -0300

    Primeiro commit

---
+++ src/br/com/Hirb/GravaPessoa.java
@@ -0,0 +1,27 @@
+package br.com.Hirb;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class GravaPessoa {		
+		public static void main(String[] args) {
+			
+			Pessoa pessoa = new Pessoa();
+			pessoa.setId(1);
+			pessoa.setNome("Teste 1");
+			
+			SessionFactory factory = new Configuration().configure().buildSessionFactory();
+			Session session = factory.openSession();
+			session.beginTransaction();
+			
+			session.save(pessoa);
+			
+			session.getTransaction().commit();
+			session.close();
+		}
+		
+	
+	
+}
